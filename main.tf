a0de17eb04ddcb118d870a4fde1c02878690b4d0
flavorplus <flavorplus@users.noreply.github.com>
2022-01-10 14:08:35 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -15,14 +15,3 @@ terraform {
     }
   }
 }
-
-provider "aws" {
-  region  = "eu-central-1"   # region is required but doesnt matter
-}
-provider "azurerm" {
-  features {}
-  subscription_id = var.subscription_id
-  tenant_id       = var.tenant_id
-  client_id       = var.client_id
-  client_secret   = var.client_secret
-}
