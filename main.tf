b780a3bad1b0e689b21cccd84e3dbd7263b5e6b1
flavorplus <flavorplus@users.noreply.github.com>
2022-01-10 14:01:20 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 terraform {
-  required_version = ">= 1.0.3"
+  required_version = ">= 1.1.3"
   required_providers {
     aws     = {
       source = "hashicorp/aws"
-      version = ">= 3.0"
+      version = ">= 3.71.0"
     }
     google  = {
       source = "hashicorp/google"
@@ -11,7 +11,18 @@ terraform {
     }
     azurerm = {
       source = "hashicorp/azurerm"
-      version = ">= 2.69.0"
+      version = ">= 2.91.0"
     }
   }
+}
+
+provider "aws" {
+  region  = "eu-central-1"   # region is required but doesnt matter
+}
+provider "azurerm" {
+  features {}
+  subscription_id = var.subscription_id
+  tenant_id       = var.tenant_id
+  client_id       = var.client_id
+  client_secret   = var.client_secret
 }
